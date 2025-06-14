40db6f69540ec197b91032fa839215e4e5b6580f
maikii <81854967+maikimilk@users.noreply.github.com>
2025-06-14 18:40:38 +0900

    Import Float in models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime
